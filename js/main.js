2a7f12a23a8d32317143bfb3f599344c45e1a4e9
beatrizsaldanha <biah_saldanha@hotmail.com>
2021-01-30 19:43:31 -0300

    teminei aula

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+
+function ConsultaCep(){
+    $(".barra-progresso").show(); 
+    var cep = document.getElementById("cep").value;
+    var url ="https://viacep.com.br/ws/" + cep + "/json/";
+   console.log(url);
+    console.log(cep);
+
+$.ajax({
+    url:url,
+    type:"GET",
+    success : function(response){
+        $("#logradouro").html(response.logradouro);
+        $("#localidade").html(response.localidade);
+        $("#bairro").html(response.bairro);
+        $("#uf").html(response.uf);
+        $("#titulo_cep").html("CEP: " + response.cep);
+        $(".cep").show(); 
+        $(".barra-progresso").hide(); 
+    }
+
+})
+}
+$(function(){
+   $(".cep").hide(); 
+   $(".barra-progresso").hide(); 
+})
